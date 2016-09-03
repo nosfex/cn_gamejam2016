5b9d91586619f1e2bf3e3a7663e8ad6a7416dec2
Gerardo Heidel <heidel.gerardo@gmail.com>
2016-09-02 21:03:29 -0300

    GH: 2 player basic movement

---
+++ New Unity Project/Assets/EventManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventManager : MonoBehaviour
+
+{
+    public delegate void MoveAction(int direction, int id);
+    public static event MoveAction moveAction;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveAction(-1, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveAction(1, 0);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            moveAction(-1, 1);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            moveAction(1, 1);
+        }
+    }
+}
---
+++ New Unity Project/Assets/Player.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+
+    public int playerID = 0;
+    public float moveScale = 1.0f;
+    Vector3 moveDir = Vector3.right;
+
+    Vector3 towards;
+    public void moveX(int direction, int id)
+    {
+        if (id != playerID)
+            return;
+        towards = new Vector3(transform.position.x  + (moveDir.x * moveScale * direction), transform.position.y, transform.position.z);
+    } 
+    // Use this for initialization
+	void Start ()
+    {
+        towards = this.transform.position;
+        EventManager.moveAction += moveX;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = Vector3.MoveTowards(this.transform.position, towards, 0.59f);
+	}
+}
